4a60d3a6dff9cc68dd62ad96892ed5494f549af4
dmitry7887 <dmitry.Gankevich@gmail.com>
2011-09-03 13:15:56 +0300

    add log Functions

--- Classes/iRemindYouAppDelegate.m
+++ Classes/iRemindYouAppDelegate.m
@@ -14,12 +14,74 @@
 
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {    
+    NSLog(@"%s", __FUNCTION__);
     
     // Override point for customization after app launch    
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
 }
 
+/* ------------------------------------------- */
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+    
+    NSLog(@"%s", __FUNCTION__);
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
+     */
+    
+    
+    NSLog(@"%s", __FUNCTION__);
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    /*
+     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
+     */
+    
+    NSLog(@"%s", __FUNCTION__);
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+    
+    NSLog(@"%s", __FUNCTION__);
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     See also applicationDidEnterBackground:.
+     */
+    
+    NSLog(@"%s", __FUNCTION__);
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+    /*
+     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
+     */
+    
+    NSLog(@"%s", __FUNCTION__);
+}
 
 - (void)dealloc {
     [viewController release];
